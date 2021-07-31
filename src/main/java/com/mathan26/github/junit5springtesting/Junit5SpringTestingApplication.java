0cc95cd3378c13508d247ce9ce077d7b498b032c
mathan26 <mathan.se@gmail.com>
2021-07-31 16:40:33 +0530

    adding spring boot project

---
+++ src/main/java/com/mathan26/github/junit5springtesting/Junit5SpringTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mathan26.github.junit5springtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5SpringTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5SpringTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathan26/github/junit5springtesting/Junit5SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathan26.github.junit5springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5SpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
